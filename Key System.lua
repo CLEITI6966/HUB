e6e12d0115a64f36ffb75721cc86d3881a7f0b86
CLEITI6966 <biungreu@gmail.com>
2025-01-25 17:58:39 -0300

    Atualizando Key System.lua

--- Key System.lua
+++ Key System.lua
@@ -9,7 +9,7 @@ local Window = OrionLib:MakeWindow({
 	IntroText = "Verificando Chave..."
 })
 
-getgenv().Key = "CLEITI6966 RO8GRFKB2SYZDPF O PIOR"
+getgenv().Key = "CLEITI6966 H0MTWVHAS1XWHLP O PIOR"
 getgenv().KeyInput = "" -- Variável para a chave inserida
 
 local Tab = Window:MakeTab({
